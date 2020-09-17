0766415fceb5f1c18def50b9e7aa6cf0061cf4a5
minjiHan95 <gorilla026@ajou.ac.kr>
2020-09-17 17:09:23 +0900

    0917

---
+++ JavaScript/function.js
@@ -0,0 +1,45 @@
+/*
+함수
+*/
+
+// function calculatePoint(x, y) {
+//     var total = x + y;
+//     var bonus = x + 100;
+// }
+
+// var calculatePoint2 = function (x, y) {
+//     var total = x + y;
+//     var bonus = x + 100;
+// }
+
+// var book={};
+// book.title = "minji";
+// book["title2"] = "minji2";
+
+// var temp = "title3";
+// book[temp] = "minji3";
+
+// console.log(book);
+
+// var obj = {
+//     book : "책"
+// };
+// console.log(obj.book);
+// console.log(obj.minji);
+// console.log(obj);
+
+// var user = {
+//     user_name : "한민지",
+//     user_age : 26,
+//     user_gender : "woman"
+// };
+
+// for(var i in user) {
+//     console.log("Attribute: "+i);
+//     console.log("Value: "+user[i]);
+// }
+
+var x = new Number("123");
+console.log(x+10);
+console.log(x.valueOf());
+
---
+++ JavaScript/jsEx.js
@@ -0,0 +1,47 @@
+// var point;
+// var book = null;
+
+// // console.log(typeof point);
+// // console.log(typeof book);
+// // console.log(book);
+
+// //논리 연산자
+// var num;
+// var x1 = 0, x2 = 2;
+// console.log(x1 || x2);
+// console.log(x1==='0');
+
+// var hol = "";
+// var eee = "";
+// for (var k = 1; k <= 50; k++) {
+//     if (k % 2 == 0) {
+//         hol += k;
+//         hol += " ";
+//     } else {
+//         eee += k;
+//         eee += " ";
+//     }
+// }
+// console.log(hol);
+// console.log(eee);
+
+// try {
+//     throw "EXCEPTION";
+//     var minji = "ssss";
+// } catch (error) {
+//     console.log(error);
+//     console.log(minji);
+
+// }
+
+// try {
+//     throw {
+//         message : "null exception",
+//         code : 404
+//     };
+// } catch (err) {
+//     console.log(err.message);
+//     console.log(err.code);
+// }
+
+
